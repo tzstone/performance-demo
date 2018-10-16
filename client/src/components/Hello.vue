5cf0f18c71439e51528b921a9d34f98cde51fc4a
flow <senb.zhuang@gmail.com>
2018-10-16 23:35:34 +0800

    init performance-demo client

---
+++ client/src/components/Hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'hello',
+    }
+  },
+  methods: {},
+  components: {},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.even {
+  color: red;
+}
+
+.odd {
+  color: green;
+}
+</style>
